f32d1f5f6838250c86b983329feaf9c51630fc71
“phrl0” <ph137163@gmail.com>
2025-01-27 16:03:14 -0300

    Commit das 3 classes

---
+++ Ex01-git+POO/cliente.java
@@ -0,0 +1,41 @@
+public class cliente {
+    private int id;
+    private String nome;
+    private String email;
+
+    public cliente(){};
+
+    public cliente(int id,String nome, String email){
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void exibir_cliente(){
+        System.out.println("ID: " + getId() + "\nNOME: " + getNome() + "\nEMAIL: " + getEmail());
+    }
+}
---
+++ Ex01-git+POO/pedido.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+
+public class pedido {
+    private int id;
+    cliente cliente;
+
+    produto produto;
+
+    public pedido(){};
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(produto produto) {
+        this.produto = produto;
+    }
+
+    public produto getPedidos() {
+        return pedidos;
+    }
+
+    public void setPedidos(produto pedidos) {
+        this.pedidos = pedidos;
+    }
+
+    public void adicionar_produto(produto produto, int quantidade){
+       public pedido(int id, produto produto,cliente cliente){
+
+        }
+    }
+
+
+
+}
