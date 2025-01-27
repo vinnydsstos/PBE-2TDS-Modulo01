74b08f19baadcdd7e3b3bc93096f9e2386aadfe6
WesleyC115 <wesley.moura03@hotmail.com>
2025-01-27 16:06:05 -0300

    commit classe cliente e pedido

--- Ex01-git+POO/Pedido.java
+++ Ex01-git+POO/Pedido.java
@@ -1,3 +1,40 @@
 public class Pedido {
 
+    private int id;
+    private String cliente;
+    private String email;
+
+    public Pedido() {
+
+    }
+
+    public Pedido(int id, String cliente, String email) {
+        this.id = id;
+        this.cliente = cliente;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
