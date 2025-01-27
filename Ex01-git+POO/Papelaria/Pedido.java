e0c4e7247fb3ce76441e269f7b0581c064fa968d
annok0 <p800h900@gmail.com>
2025-01-27 16:01:59 -0300

    p

--- Ex01-git+POO/Papelaria/Pedido.java
+++ Ex01-git+POO/Papelaria/Pedido.java
@@ -4,3 +4,21 @@ public class Pedido {
     public int Id;
     public Cliente cliente;
     ArrayList<Produtos> produtos = new ArrayList();
+
+    //métodos
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+}
