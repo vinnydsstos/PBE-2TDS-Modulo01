a52d0f147a17a75b9cf0a3267a231ce9e0c77402
gallom1227 <miguelgallo1227@gmail.com>
2025-01-27 15:41:41 -0300

    Ain Mykael

---
+++ Ex01-git+POO/Produto.java
@@ -0,0 +1,38 @@
+public class Produto {
+    private int id;
+    public String Nome;
+    public double preco;
+    public int quantidade_estoque;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String nome) {
+        Nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidade_estoque() {
+        return quantidade_estoque;
+    }
+
+    public void setQuantidade_estoque(int quantidade_estoque) {
+        this.quantidade_estoque = quantidade_estoque;
+    }
+}
