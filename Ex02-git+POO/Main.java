88e6aa3df5d4adeb5f16bd247821c3a18454b933
Pedro henrique rodrigues leme <151878192+PhrL0@users.noreply.github.com>
2025-01-29 16:15:52 -0300

    Revert "Classes Prontas"

--- Ex02-git+POO/Main.java
+++ Ex02-git+POO/Main.java
@@ -1,18 +1,3 @@
- main
-public class Main {
-    public static void main(String[] args) {
-        Quarto quarto1 = new Quarto(1, "Solteiro", 100.0);
-        Hospede hospede1 = new Hospede(1, "Maria Oliveira", "11 99999-1234");
-
-
-        Reserva reserva1 = new Reserva(1, hospede1, quarto1, 3);
-
-
-        reserva1.finalizarReserva();
-        System.out.println(reserva1.exibirResumo());
-    }
-}
-
 import java.util.Scanner;
 
 public class Main {
@@ -67,4 +52,3 @@ public class Main {
     }
 
 }
- main
