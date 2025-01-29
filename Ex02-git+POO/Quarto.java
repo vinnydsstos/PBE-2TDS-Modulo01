b517dc5073c37166462cdb0d21606c3bb4465068
“[ViniciusDev00]” <[vinicius.biancolini.tds24@gmail.com]>
2025-01-29 14:53:47 -0300

    Quarto

---
+++ Ex02-git+POO/Quarto.java
@@ -0,0 +1,68 @@
+public class Quarto {
+
+    private int id;
+    private String tipo;
+    private float preco = 80;
+    public boolean disponivel;
+
+    Quarto() {
+
+    }
+
+    Quarto(int id, String tipo, float preco, boolean disponivel) {
+        this.id = id;
+        this.tipo = tipo;
+        this.preco = preco;
+        this.disponivel = disponivel;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+
+    //marcar_indisponivel(): Altera o status de disponibilidade para False.
+    //marcar_disponivel(): Altera o status de disponibilidade para True.
+    //exibir_detalhes(): Mostra os dados do quarto.
+
+    public void marcar_indisponivel() {
+        if (disponivel = true) {
+            System.out.println("Quarto disponível");
+        } else {
+            System.out.println("Quarto não disponivel");
+        }
+    }
+
+    public void exibir_detalhes() {
+        System.out.println("id: " + id + "tipo" + tipo + "preço" + preco + "disponível" + disponivel);
+
+    }
+}
