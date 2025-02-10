22f6d15be202e80b2f094f8cf949a70227f6a455
WesleyC115 <wesley.moura03@hotmail.com>
2025-02-10 16:50:58 -0300

    commit1

---
+++ src/main/java/com/senai/demo/FirstendpointApplication.java
@@ -0,0 +1,13 @@
+package com.senai.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstendpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstendpointApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/senai/demo/Hello.java
@@ -0,0 +1,42 @@
+package com.senai.demo;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/usuario")
+public class Hello {
+
+    // Instancia o objeto com valores padrão
+    private Usuario usuario = new Usuario("Wesley", "22222222222", "16987654321");
+
+    // GET - retorna os dados do usuário
+    @GetMapping
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    // POST - cria ou substitui o usuário com os dados enviados na requisição
+    @PostMapping
+    public Usuario criarUsuario(@RequestBody Usuario novoUsuario) {
+        this.usuario = novoUsuario;
+        return this.usuario;
+    }
+
+    // PUT - atualiza os dados do usuário
+    @PutMapping
+    public Usuario atualizarUsuario(@RequestBody Usuario usuarioAtualizado) {
+        if (this.usuario != null) {
+            this.usuario.setNome(usuarioAtualizado.getNome());
+            this.usuario.setCpf(usuarioAtualizado.getCpf());
+            this.usuario.setTelefone(usuarioAtualizado.getTelefone());
+        }
+        return this.usuario;
+    }
+
+    // DELETE - "deleta" o usuário (neste exemplo, apenas setamos o objeto como null)
+    @DeleteMapping
+    public String deletarUsuario() {
+        this.usuario = null;
+        return "Usuário deletado!";
+    }
+}
